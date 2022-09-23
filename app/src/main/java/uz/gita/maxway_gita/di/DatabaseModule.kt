ca07657fe80d739bb75dd8a97dfd9767d7b177d7
jamshidisoqov <isoqovjamshid01@gmail.com>
2022-09-24 01:09:52 +0500

    Initial commit

---
+++ app/src/main/java/uz/gita/maxway_gita/di/DatabaseModule.kt
@@ -0,0 +1,5 @@
+package uz.gita.maxway_gita.di
+
+// Created by Jamshid Isoqov an 9/24/2022
+object DatabaseModule {
+}
---
+++ app/src/main/java/uz/gita/maxway_gita/presentation/screens/basket/BasketScreen.kt
@@ -0,0 +1,5 @@
+package uz.gita.maxway_gita.presentation.screens.basket
+
+// Created by Jamshid Isoqov an 9/24/2022
+class BasketScreen {
+}
