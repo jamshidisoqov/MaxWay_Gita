9dd2e11f22e7b0e96dcb179aa786aa6633b691b9
Arzigul-Developer <arzigul.nazarbaeva1@s.amity.edu>
2022-09-24 12:31:44 +0500

    -library added to gradle

--- app/src/main/java/uz/gita/maxway_gita/presentation/screens/basket/BasketScreen.kt
+++ app/src/main/java/uz/gita/maxway_gita/presentation/screens/basket/BasketScreen.kt
@@ -1,8 +1,12 @@
 package uz.gita.maxway_gita.presentation.screens.basket
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import cafe.adriel.voyager.androidx.AndroidScreen
+import com.google.accompanist.pager.*
 import uz.gita.maxway_gita.presentation.theme.MaxWayGitaTheme
 
 // Created by Jamshid Isoqov an 9/24/2022
@@ -18,7 +22,29 @@ class BasketScreen : AndroidScreen() {
 
 @Composable
 fun BasketScreenContent() {
+    Surface {
+        Column {
+            Text(text = "Mening zaqaslarim")
+            TabLayout()
+        }
+    }
+}
 
+@Composable
+fun TabLayout() {
+
+}
+
+@ExperimentalPagerApi
+@Composable
+fun TabsContent(pagerState: PagerState) {
+    HorizontalPager(state = pagerState) { page ->
+        when (page) {
+            0 -> BasketScreen()
+            1 -> BasketScreen()
+            2 -> BasketScreen()
+        }
+    }
 }
 
 @Composable
