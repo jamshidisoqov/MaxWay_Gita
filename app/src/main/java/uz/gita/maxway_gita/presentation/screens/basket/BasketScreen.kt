a471c75baa656d9e67619417a49e16060bf39fb4
Arzigul-Developer <arzigul.nazarbaeva1@s.amity.edu>
2022-09-24 11:45:50 +0500

    -bug fixed in gradle

--- app/src/main/java/uz/gita/maxway_gita/presentation/screens/basket/BasketScreen.kt
+++ app/src/main/java/uz/gita/maxway_gita/presentation/screens/basket/BasketScreen.kt
@@ -1,5 +1,30 @@
 package uz.gita.maxway_gita.presentation.screens.basket
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import cafe.adriel.voyager.androidx.AndroidScreen
+import uz.gita.maxway_gita.presentation.theme.MaxWayGitaTheme
+
 // Created by Jamshid Isoqov an 9/24/2022
-class BasketScreen {
+class BasketScreen : AndroidScreen() {
+
+    @Composable
+    override fun Content() {
+        MaxWayGitaTheme {
+            BasketScreenContent()
+        }
+    }
+}
+
+@Composable
+fun BasketScreenContent() {
+
+}
+
+@Composable
+@Preview
+fun BasketScreenPreview() {
+    MaxWayGitaTheme {
+        BasketScreenContent()
+    }
 }
