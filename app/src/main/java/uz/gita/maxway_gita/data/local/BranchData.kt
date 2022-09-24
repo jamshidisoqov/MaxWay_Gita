d7e2d1993fe03ec7df2c95229b7cfdd83d42a30c
jamshidisoqov <isoqovjamshid01@gmail.com>
2022-09-24 22:23:12 +0500

    add firestore

---
+++ app/src/main/java/uz/gita/maxway_gita/data/local/BranchData.kt
@@ -0,0 +1,6 @@
+package uz.gita.maxway_gita.data.local
+
+// Created by Jamshid Isoqov an 9/24/2022
+data class BranchData(
+    val address:String
+)
---
+++ app/src/main/java/uz/gita/maxway_gita/data/local/ProductData.kt
@@ -0,0 +1,10 @@
+package uz.gita.maxway_gita.data.local
+
+// Created by Jamshid Isoqov an 9/24/2022
+data class ProductData(
+    val productId: Int,
+    val name: String,
+    val imageUrl: String,
+    val price: Double,
+    val info: String
+)
