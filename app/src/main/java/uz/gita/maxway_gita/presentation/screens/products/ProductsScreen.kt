d5ff54685152f113dbf342b06aed898ff85e2cd5
jamshidisoqov <isoqovjamshid01@gmail.com>
2022-09-25 14:18:18 +0500

    test bug fixed

--- app/src/main/java/uz/gita/maxway_gita/presentation/screens/products/ProductsScreen.kt
+++ app/src/main/java/uz/gita/maxway_gita/presentation/screens/products/ProductsScreen.kt
@@ -1,5 +1,24 @@
 package uz.gita.maxway_gita.presentation.screens.products
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import cafe.adriel.voyager.androidx.AndroidScreen
+
 // Created by Jamshid Isoqov an 9/24/2022
-class ProductsScreen {
+class ProductsScreen : AndroidScreen() {
+    @Composable
+    override fun Content() {
+
+    }
+}
+
+@Composable
+private fun ProductsScreenContent() {
+
+}
+
+@Composable
+@Preview
+fun ProductsScreenContentPreview() {
+
 }
