52fb6d5631bbf6706a17a0e4413a636ffb93f8ae
Gilfoyle-dev <bek.droid.dev@gmail.com>
2022-09-25 13:41:28 +0500

    CheckoutUIState, CheckoutIntent, viewModel UPDATED

--- app/src/main/java/uz/gita/maxway_gita/presentation/screens/order/checkout/CheckoutScreen.kt
+++ app/src/main/java/uz/gita/maxway_gita/presentation/screens/order/checkout/CheckoutScreen.kt
@@ -1,16 +1,20 @@
 package uz.gita.maxway_gita.presentation.screens.order.checkout
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import cafe.adriel.voyager.androidx.AndroidScreen
 import cafe.adriel.voyager.hilt.getViewModel
 import org.orbitmvi.orbit.compose.collectAsState
+import uz.gita.maxway_gita.R
 import uz.gita.maxway_gita.presentation.screens.order.checkout.intent.CheckoutIntent
 import uz.gita.maxway_gita.presentation.screens.order.checkout.ui_state.CheckoutUiState
 import uz.gita.maxway_gita.presentation.screens.order.checkout.viewmodels.CheckoutViewModel
 import uz.gita.maxway_gita.presentation.screens.order.checkout.viewmodels.impl.CheckoutViewModelImpl
+import uz.gita.maxway_gita.utils.AppBar
 
 // Yusupov Bekzod.
 class CheckoutScreen: AndroidScreen() {
@@ -28,7 +32,7 @@ private fun MainScreenContent(
     uiState: CheckoutUiState,
     eventDispatcher: (CheckoutIntent) -> Unit
 ) {
-    Surface(modifier = Modifier.fillMaxSize()) {
-
+    Surface(modifier = Modifier.fillMaxSize().background(Color(0xFFF8F6FA))) {
+        AppBar(onEventDispatcher = eventDispatcher)//title ham bersa bo'ladi man default yozib qo'ygganim uchun bu yerda so'ramadi.
     }
 }
--- app/src/main/java/uz/gita/maxway_gita/utils/ComposeableFunctions.kt
+++ app/src/main/java/uz/gita/maxway_gita/utils/ComposeableFunctions.kt
@@ -1,31 +1,50 @@
 package uz.gita.maxway_gita.utils
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.BackdropScaffoldState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.material.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import uz.gita.maxway_gita.R
+import uz.gita.maxway_gita.presentation.screens.order.checkout.intent.CheckoutIntent
 
 @Composable
 fun AppBar(
-    title:String
+    title: String = "Checkout Order",
+    onEventDispatcher:(CheckoutIntent)->Unit
 ) {
-    Box(modifier = Modifier
-        .fillMaxWidth()
-        .height(60.dp)) {
-        Row(modifier = Modifier.fillMaxSize()) {
-            Image(painter = painterResource(id = R.drawable.ic_baseline_arrow_back_ios_24), modifier = Modifier.fillMaxHeight(), contentDescription = "back")
-        }
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(60.dp)
+            .background(Color.White),
+    ) {
+        Image(
+            contentDescription = "back",
+            painter = painterResource(id = R.drawable.ic_baseline_arrow_back_ios_24),
+            modifier = Modifier
+                .clickable { onEventDispatcher(CheckoutIntent.back) }
+                .padding(18.dp),
+        )
+        Text(text = title, modifier = Modifier.align(Alignment.Center), style = TextStyle(fontSize = 17.sp, fontWeight = FontWeight.SemiBold))
     }
 }
 
 @Composable
 @Preview
 fun AppbarPre() {
-    AppBar("hello")
+//    AppBar()
 }
